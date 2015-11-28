a2a9cb8fd8225024b64eba9a3258958d79913274
Marvin Pinto <marvin@pinto.im>
2015-11-28 11:25:58 -0500

    Mock out the load_config method

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.plugin_name|lower}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.plugin_name|lower}}.py
@@ -1,9 +1,15 @@
 import asynctest
-from {{ cookiecutter.package_name|lower }}.{{cookiecutter.plugin_name|lower}} import {{ cookiecutter.plugin_name }}
+from asynctest.mock import patch
+
 
 class Test{{cookiecutter.plugin_name}}(asynctest.TestCase):
 
     def setUp(self):
+        patcher1 = patch('{{ cookiecutter.package_name|lower }}.{{cookiecutter.plugin_name|lower}}.{{ cookiecutter.plugin_name }}.load_config')  # NOQA
+        self.addCleanup(patcher1.stop)
+        self.mock_load_config = patcher1.start()
+
+        from {{ cookiecutter.package_name|lower }}.{{cookiecutter.plugin_name|lower}} import {{ cookiecutter.plugin_name }}
         test_plug = {{ cookiecutter.plugin_name }}()  # NOQA
 
     def tearDown(self):
